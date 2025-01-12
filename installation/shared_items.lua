c8acb0d8267f9a69f610d588ec2b26705a005fca
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-01-12 19:13:51 +0000

    Update shared_items.lua

--- installation/shared_items.lua
+++ installation/shared_items.lua
@@ -1,2 +1,2 @@
 	-- mobile dj
-	['phonograph']					= {['name'] = 'phonograph', 			  	  		['label'] = 'phonograph', 				['weight'] = 5000, 		['type'] = 'item', 		['image'] = 'phonograph.png', 				['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,		['combinable'] = nil,   ['description'] = 'spin the decks'},
+	['phonograph']					= {['name'] = 'phonograph', 			  	  		['label'] = 'phonograph', 				['weight'] = 5000, 		['type'] = 'item', 		['image'] = 'phonograph.png', 				['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,		['combinable'] = nil,   ['description'] = 'music'},
